f2746370c5cdaec9ed1521fbe0630f159551561c
thebrianemory <the@brianemory.com>
2017-08-24 22:15:11 -0700

    Major refactor of create action

--- lib/catcasts_phx1_3_web/controllers/video_controller.ex
+++ lib/catcasts_phx1_3_web/controllers/video_controller.ex
@@ -15,55 +15,26 @@ defmodule CatcastsPhx13Web.VideoController do
   end
 
   def create(conn, %{"video" => video_params}) do
-    # Sets regex to nil if invalid URL to make sure we only get valid YouTube links
-    regex = Regex.run(~r/(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/, video_params["video_id"])
+    case has_valid_regex?(video_params) do
+      nil ->
+        changeset = Video.changeset(%Video{}, video_params)
 
-    if regex == nil do
-      changeset = Video.changeset(%Video{}, video_params)
+        conn
+        |> put_flash(:error, "Invalid YouTube URL")
+        |> render("new.html", changeset: changeset)
+      regex ->
+        video_id = get_video_id(regex)
 
-      conn
-      |> put_flash(:error, "Invalid YouTube URL")
-      |> render("new.html", changeset: changeset)
-    else
-      # Grab only the video ID from the submitted YouTube link
-      video_id = List.first(tl(regex))
+        video_data = get_json_data(video_id)
+               |> decode_json_data()
+               |> get_video_data()
 
-      # Submit our info to the YouTube API and get back the JSON
-      json_data = HTTPoison.get! "https://www.googleapis.com/youtube/v3/videos?id=#{video_id}&key=#{System.get_env("YOUTUBE_API_KEY")}&part=snippet,statistics,contentDetails&fields=items(id,snippet(title,thumbnails(high)),statistics(viewCount),contentDetails(duration))"
+        video_attrs = get_formatted_time(video_data)
+                      |> create_video_attrs(video_data)
 
-      # Decode the JSON
-      data = Poison.decode!(json_data.body, keys: :atoms)
+        changeset = create_changeset(conn, video_attrs)
 
-      # Grab our items from the JSON list within our data
-      items = hd(data.items)
-
-      # Convert the duration into a human readable format
-      length_regex = tl(Regex.run(~r/PT(\d+H)?(\d+M)?(\d+S)?/, items.contentDetails.duration))
-      duration = get_formatted_time(length_regex)
-
-      # The information we need to create our video
-      valid_attrs = %{duration: duration, thumbnail: items.snippet.thumbnails.high.url,
-                      title: items.snippet.title, video_id: items.id,
-                      view_count: String.to_integer(items.statistics.viewCount)}
-
-      # Creates are changeset and builds the association with the current user
-      changeset = conn.assigns.user
-      |> Ecto.build_assoc(:videos)
-      |> Video.changeset(valid_attrs)
-
-      # Creates our video in the database if it does not already exist
-      # If it fails to create at this point, that means it already exists. Redirect to its show page
-      case CatcastsPhx13.Repo.insert(changeset) do
-        {:ok, video} ->
-          conn
-          |> put_flash(:info, "Video created successfully.")
-          |> redirect(to: video_path(conn, :show, video))
-        {:error, _video} ->
-          video = CatcastsPhx13.Video |> CatcastsPhx13.Repo.get_by(video_id: video_id)
-          conn
-          |> put_flash(:info, "Video has already been created.")
-          |> redirect(to: video_path(conn, :show, video))
-      end
+        create_or_redirect?(changeset, conn, video_id)
     end
   end
 
@@ -81,11 +52,59 @@ defmodule CatcastsPhx13Web.VideoController do
     |> redirect(to: video_path(conn, :index))
   end
 
-  defp get_formatted_time(duration) do
+  defp has_valid_regex?(video_params) do
+    Regex.run(~r/(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/, video_params["video_id"])
+  end
+
+  defp get_video_id(regex) do
+    List.first(tl(regex))
+  end
+
+  defp get_json_data(video_id) do
+    HTTPoison.get! "https://www.googleapis.com/youtube/v3/videos?id=#{video_id}&key=#{System.get_env("YOUTUBE_API_KEY")}&part=snippet,statistics,contentDetails&fields=items(id,snippet(title,thumbnails(high)),statistics(viewCount),contentDetails(duration))"
+  end
+
+  defp decode_json_data(json_data) do
+    Poison.decode!(json_data.body, keys: :atoms)
+  end
+
+  defp get_video_data(video) do
+    hd(video.items)
+  end
+
+  defp get_formatted_time(video_data) do
+    duration = tl(Regex.run(~r/PT(\d+H)?(\d+M)?(\d+S)?/, video_data.contentDetails.duration))
+
     [hours, minutes, seconds] =
       for x <- duration, do: hd(Regex.run(~r{\d+}, x) || ["0"]) |> String.to_integer
 
     {_status, time} = Time.new(hours, minutes, seconds)
     Time.to_string(time)
+  end
+
+  defp create_video_attrs(duration, video_data) do
+    %{duration: duration, thumbnail: video_data.snippet.thumbnails.high.url,
+      title: video_data.snippet.title, video_id: video_data.id,
+      view_count: String.to_integer(video_data.statistics.viewCount)}
+  end
+
+  defp create_changeset(conn, video_attrs) do
+    conn.assigns.user
+    |> Ecto.build_assoc(:videos)
+    |> Video.changeset(video_attrs)
+  end
+
+  defp create_or_redirect?(changeset, conn, video_id) do
+    case CatcastsPhx13.Repo.insert(changeset) do
+      {:ok, video} ->
+        conn
+        |> put_flash(:info, "Video created successfully.")
+        |> redirect(to: video_path(conn, :show, video))
+      {:error, _video} ->
+        video = CatcastsPhx13.Video |> CatcastsPhx13.Repo.get_by(video_id: video_id)
+        conn
+        |> put_flash(:info, "Video has already been created.")
+        |> redirect(to: video_path(conn, :show, video))
+    end
   end
 end
